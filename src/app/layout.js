111be8d63a645190f6d43b4b6fb1b251200dab60
jdmkd <jdmkd49@gmail.com>
2025-06-30 13:39:39 -0700

    feat: integrate Microsoft Clarity analytics
    - Added Microsoft Clarity script to src/app/layout.js for user behavior analytics tracking.

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer/Footer";
 import SocialLinks from "@/components/SocialLinks/SocialLinks";
 import RouteProgress from "@/widgets/RouteProgress";
 import PageWrapper from "@/widgets/PageWrapper";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,19 @@ export default function RootLayout({ children }) {
         {children}
       </PageWrapper>
       <Footer />
+      <Script
+        id="microsoft-clarity-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src=\"https://www.clarity.ms/tag/\"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, \"clarity\", \"script\", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY}");
+          `,
+        }}
+      />
       </body>
     </html>
   );
